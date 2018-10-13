0667c244f8c660b992a04eeb37dc1fe3255711a6
xiaodi528 <eDCqvBsffku3FmoKAXFPapEk>
2018-10-13 10:10:06 -0700

    First commit

---
+++ SeatsFinderBot/SeatsFinderBot/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace SeatsFinderBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subjectText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numberText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sectionText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.userText = new System.Windows.Forms.TextBox();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.reservedText = new System.Windows.Forms.TextBox();
+            this.GUID = new System.Windows.Forms.Label();
+            this.guidText = new System.Windows.Forms.TextBox();
+            this.swapRadio = new System.Windows.Forms.RadioButton();
+            this.addRadio = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // subjectText
+            // 
+            this.subjectText.Location = new System.Drawing.Point(689, 165);
+            this.subjectText.Name = "subjectText";
+            this.subjectText.Size = new System.Drawing.Size(236, 35);
+            this.subjectText.TabIndex = 0;
+            this.subjectText.TextChanged += new System.EventHandler(this.subjectText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(547, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Subject";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(547, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number";
+            // 
+            // numberText
+            // 
+            this.numberText.Location = new System.Drawing.Point(689, 246);
+            this.numberText.Name = "numberText";
+            this.numberText.Size = new System.Drawing.Size(236, 35);
+            this.numberText.TabIndex = 3;
+            this.numberText.TextChanged += new System.EventHandler(this.numberText_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(547, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Section";
+            // 
+            // sectionText
+            // 
+            this.sectionText.Location = new System.Drawing.Point(689, 328);
+            this.sectionText.Name = "sectionText";
+            this.sectionText.Size = new System.Drawing.Size(236, 35);
+            this.sectionText.TabIndex = 5;
+            this.sectionText.TextChanged += new System.EventHandler(this.sectionText_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 1152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(374, 69);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(547, 574);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "User Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(547, 658);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Password";
+            // 
+            // userText
+            // 
+            this.userText.Location = new System.Drawing.Point(689, 571);
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(236, 35);
+            this.userText.TabIndex = 9;
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(689, 655);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(236, 35);
+            this.passwordText.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(551, 416);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Reserved";
+            // 
+            // reservedText
+            // 
+            this.reservedText.Location = new System.Drawing.Point(689, 413);
+            this.reservedText.Name = "reservedText";
+            this.reservedText.Size = new System.Drawing.Size(236, 35);
+            this.reservedText.TabIndex = 12;
+            // 
+            // GUID
+            // 
+            this.GUID.AutoSize = true;
+            this.GUID.Location = new System.Drawing.Point(551, 815);
+            this.GUID.Name = "GUID";
+            this.GUID.Size = new System.Drawing.Size(58, 24);
+            this.GUID.TabIndex = 13;
+            this.GUID.Text = "GUID";
+            // 
+            // guidText
+            // 
+            this.guidText.Location = new System.Drawing.Point(689, 812);
+            this.guidText.Name = "guidText";
+            this.guidText.Size = new System.Drawing.Size(236, 35);
+            this.guidText.TabIndex = 14;
+            // 
+            // swapRadio
+            // 
+            this.swapRadio.AutoSize = true;
+            this.swapRadio.Location = new System.Drawing.Point(555, 972);
+            this.swapRadio.Name = "swapRadio";
+            this.swapRadio.Size = new System.Drawing.Size(89, 28);
+            this.swapRadio.TabIndex = 15;
+            this.swapRadio.TabStop = true;
+            this.swapRadio.Text = "Swap";
+            this.swapRadio.UseVisualStyleBackColor = true;
+            this.swapRadio.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // addRadio
+            // 
+            this.addRadio.AutoSize = true;
+            this.addRadio.Location = new System.Drawing.Point(848, 972);
+            this.addRadio.Name = "addRadio";
+            this.addRadio.Size = new System.Drawing.Size(77, 28);
+            this.addRadio.TabIndex = 16;
+            this.addRadio.TabStop = true;
+            this.addRadio.Text = "Add";
+            this.addRadio.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1528, 1502);
+            this.Controls.Add(this.addRadio);
+            this.Controls.Add(this.swapRadio);
+            this.Controls.Add(this.guidText);
+            this.Controls.Add(this.GUID);
+            this.Controls.Add(this.reservedText);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.userText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sectionText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numberText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.subjectText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox subjectText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox numberText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sectionText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox userText;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox reservedText;
+        private System.Windows.Forms.Label GUID;
+        private System.Windows.Forms.TextBox guidText;
+        private System.Windows.Forms.RadioButton swapRadio;
+        private System.Windows.Forms.RadioButton addRadio;
+    }
+}
+
---
+++ SeatsFinderBot/SeatsFinderBot/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Net;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeatsFinderBot
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subjectText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numberText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sectionText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static bool IsElementPresent(ChromeDriver driver, By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private static String SuperPowerAutomates_add(String username, String password, String sectionNum)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--incognito");
+
+            var driver = new ChromeDriver(options);
+
+            driver.Navigate().GoToUrl("https://go.oasis.asu.edu/addclass/?STRM=2197&ACAD_CAREER=GRAD");
+
+            //Login
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//frame[@src='https://weblogin.asu.edu/cgi-bin/login?callapp=https%3A//go.oasis.asu.edu/waitframeset.html%3Fdelay%3D3500%26url%3Dhttps%253A//cs.oasis.asu.edu/asucsprd/golink/%253F/EMPLOYEE/PSFT_ASUCSPRD/s/WEBLIB_ASU_SA.ASU_SA_ISCRIPT.FieldFormula.IScript_SA%253FURL%253D/EMPLOYEE/PSFT_ASUCSPRD/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL%25253FSTRM%25253D2187%252526ACAD_CAREER%25253DGRAD%252526Page%25253DSSR_SSENRL_ADD%252526Action%25253DA%252526INSTITUTION%25253DASU00%252526golink%25253DY']")));
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("submit")).Click();
+
+            //...
+            driver.SwitchTo().Frame(0);
+            driver.SwitchTo().Frame(0);
+            driver.SwitchTo().Frame(2);
+
+            //Empty Shopping Cards
+            while (IsElementPresent(driver, By.Id("P_DELETE$0")))
+            {
+                driver.FindElement(By.Id("P_DELETE$0")).Click();
+            }
+
+            Thread.Sleep(2000);
+
+            //Enter Section Number
+            driver.FindElement(By.Id("DERIVED_REGFRM1_CLASS_NBR")).SendKeys(sectionNum);
+            driver.FindElement(By.Id("DERIVED_REGFRM1_SSR_PB_ADDTOLIST2$9$")).Click();
+
+            //Add Class
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("DERIVED_CLS_DTL_NEXT_PB$280$")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("DERIVED_REGFRM1_LINK_ADD_ENRL$82$")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("DERIVED_REGFRM1_SSR_PB_SUBMIT")).Click();
+
+            //Get Status
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("win0divDERIVED_REGFRM1_SSR_STATUS_LONG$200$$0"));
+            var status = driver.PageSource.ToString();
+            var FinalStatus = "";
+            System.Diagnostics.Debug.WriteLine("status: " + driver.PageSource);
+            if (status.Contains("Error"))
+            {
+                System.Diagnostics.Debug.WriteLine("status: Error");
+                FinalStatus = "Error";
+            }
+            else if (status.Contains("Success"))
+            {
+                System.Diagnostics.Debug.WriteLine("status: Success");
+                FinalStatus = "Success";
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("status: Unknown");
+                FinalStatus = "Unknown";
+            }
+            System.Diagnostics.Debug.WriteLine("FinalStatus: " + FinalStatus);
+            return FinalStatus;
+        }
+
+        private static String SuperPowerAutomates_swap(String username, String password, String sectionNum)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--incognito");
+
+            var driver = new ChromeDriver(options); 
+
+            driver.Navigate().GoToUrl("https://go.oasis.asu.edu/swapclass/?STRM=2197&ACAD_CAREER=GRAD&ASU_CLASS_NBR=" + sectionNum);
+
+            //Login
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//frame[@src='https://weblogin.asu.edu/cgi-bin/login?callapp=https%3A//go.oasis.asu.edu/waitframeset.html%3Fdelay%3D3500%26url%3Dhttps%253A//cs.oasis.asu.edu/asucsprd/golink/%253F/EMPLOYEE/PSFT_ASUCSPRD/s/WEBLIB_ASU_SA.ASU_SA_ISCRIPT.FieldFormula.IScript_SA%253FURL%253D/EMPLOYEE/PSFT_ASUCSPRD/c/SA_LEARNER_SERVICES.SSR_SSENRL_SWAP.GBL%25253FSTRM%25253D2187%252526ACAD_CAREER%25253DGRAD%252526ASU_CLASS_NBR%25253D" + sectionNum + "%252526Page%25253DSSR_SSENRL_SWAP%252526Action%25253DA%252526INSTITUTION%25253DASU00%252526golink%25253DY']")));
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
+            driver.FindElement(By.ClassName("submit")).Click();
+
+            //...
+            driver.SwitchTo().Frame(0);
+            driver.SwitchTo().Frame(0);
+            driver.SwitchTo().Frame(2);
+
+            //Empty Shopping Cards
+            while (IsElementPresent(driver, By.Id("P_DELETE$0")))
+            {
+                driver.FindElement(By.Id("P_DELETE$0")).Click();
+            }
+
+            //Thread.Sleep(2000);
+
+            // wwSelect class from dropdownlist
+            var dropdownlist = driver.FindElement(By.Id("DERIVED_REGFRM1_DESCR50$225$"));
+
+            // create select element object 
+            var selectElement = new SelectElement(dropdownlist);
+
+            // select by value
+            selectElement.SelectByValue("79036");
+
+            // Click Enter
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("DERIVED_REGFRM1_SSR_PB_ADDTOLIST2$106$")).Click();
+
+            // Click Next
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("DERIVED_CLS_DTL_NEXT_PB")).Click();
+
+            // Click Finish Swaping
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("DERIVED_REGFRM1_SSR_PB_SUBMIT")).Click();
+
+            //Get Status
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("win0divDERIVED_REGFRM1_SSR_STATUS_LONG$200$$0"));
+            var status = driver.PageSource.ToString();
+            var FinalStatus = "";
+            System.Diagnostics.Debug.WriteLine("status: " + driver.PageSource);
+            if (status.Contains("Error"))
+            {
+                System.Diagnostics.Debug.WriteLine("status: Error");
+                FinalStatus = "Error";
+            }
+            else if (status.Contains("Success"))
+            {
+                System.Diagnostics.Debug.WriteLine("status: Success");
+                FinalStatus = "Success";
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("status: Unknown");
+                FinalStatus = "Unknown";
+            }
+            System.Diagnostics.Debug.WriteLine("FinalStatus: " + FinalStatus);
+            return FinalStatus;
+        }
+
+        private void runAction()
+        {
+            WebClient client = new WebClient();
+
+            //test 
+            //String Test = SuperPowerAutomates_swap("dxiao6", "Xg7DuWfTo6L7aYcukqKg");
+
+            //    01/05/2009 6:01 PM
+            string currentTime = DateTime.Now.ToString("MM\\/dd\\/yyyy h\\:mm tt");
+
+            string sec = sectionText.Text;
+            string res = reservedText.Text;
+            string gui = guidText.Text;
+
+
+            try
+            {
+                string classStatus = client.DownloadString("https://seatsfinderweb.azurewebsites.net/api/WebAPI?checkClassStatus=true&prefix=&number=&location=Tempe&term=Spring+2019&sectionNumber=" + sec + "&reservedSeats=" + res);
+
+                System.Diagnostics.Debug.WriteLine("classStatus: " + classStatus);
+
+                if (classStatus == "\"FULL\"")
+                {
+                    // Do nothing
+                    client.DownloadString("http://seatsfinderweb.azurewebsites.net/api/WebAPI?GetSuperPowerVMTaskSchedulerStatus=true&guid=" + gui + "&taskID=" + sec + "&time=" + currentTime + "&status=FULL");
+
+                }
+                else if (classStatus == "\"OPEN\"")
+                {
+                    if (swapRadio.Checked)
+                    {
+                        String FinalStatus = SuperPowerAutomates_swap(userText.Text, passwordText.Text, sectionText.Text);
+
+                        client.DownloadString("http://seatsfinderweb.azurewebsites.net/api/WebAPI?GetSuperPowerVMTaskSchedulerStatus=true&guid=" + gui + "&taskID=" + sec + "&time=" + currentTime + "&status=OPEN");
+
+                        // [Error, Success, Unknown]
+                        System.Diagnostics.Debug.WriteLine(FinalStatus);
+                    }
+                    else if (addRadio.Checked)
+                    {
+                        String FinalStatus = SuperPowerAutomates_add(userText.Text, passwordText.Text, sectionText.Text);
+
+                        client.DownloadString("http://seatsfinderweb.azurewebsites.net/api/WebAPI?GetSuperPowerVMTaskSchedulerStatus=true&guid=" + gui + "&taskID=" + sec + "&time=" + currentTime + "&status=OPEN");
+
+                        // [Error, Success, Unknown]
+                        System.Diagnostics.Debug.WriteLine(FinalStatus);
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                //GetSuperPowerVMTaskSchedulerStatus(string guid, string taskID, string time, bool status)
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception Catched.");
+
+                try
+                {
+                    client.DownloadString("http://seatsfinderweb.azurewebsites.net/api/WebAPI?GetSuperPowerVMTaskSchedulerStatus=true&guid=" + gui + "&taskID=" + sec + "&time=" + currentTime + "&status=Exception");
+                }
+                catch (Exception ex2)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex2);
+                }
+
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(1);
+
+            var timer = new System.Threading.Timer(
+            e1 => runAction(),
+            null,
+            TimeSpan.Zero,
+            TimeSpan.FromSeconds(10));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
